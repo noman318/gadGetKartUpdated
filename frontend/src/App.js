eb3ea9f09456874c8b7565c124e1da3a886952fa
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2023-12-17 16:24:35 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to MyShop</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
